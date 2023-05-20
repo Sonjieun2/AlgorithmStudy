3905c656471d0750dea200d8556400a4ae0440b5
[Sonjieun2] <[jeun2e2@gmail.com]>
2023-05-21 00:06:39 +0900

    0518

---
+++ Baekjoon/0518/2941.py
@@ -0,0 +1,12 @@
+word = input().lower()
+croatia = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+
+for i in croatia :
+    if i in word :
+        word = word.replace(i, "*")
+    
+    else :
+        continue
+
+print(word)
+print(len(word))
