23b2ebbfbe84afa5fbe0fa381bf0632d09857805
[Sonjieun2] <[jeun2e2@gmail.com]>
2023-05-26 21:21:37 +0900

    change

--- Baekjoon/0526/11005.py
+++ Baekjoon/0526/11005.py
@@ -7,7 +7,7 @@ while num >= n :
     result.append(str[int(num%n)])
     num = num//n
 
-result.append(str[int(num%n)])
+result.append(str[num])
 
 for i in range(len(result)-1, -1, -1) :
     print(result[i], end="")
